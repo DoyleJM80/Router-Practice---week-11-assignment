fc82b517ee803c7a13d9d5c1a42b3de619b81150
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-07-31 09:38:27 -0400

    added activeClassName to all routes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,11 +5,11 @@ export default class Navigation extends Component {
   render() {
     return(
       <div className="App one">
-        <div><button className="btn"><NavLink exact to="/">Home</NavLink></button></div>
-        <div><button className="btn"><NavLink to="/about">About</NavLink></button></div>
-        <div><button className="btn"><NavLink to="/portfolio">Portfolio</NavLink></button></div>
-        <div><button className="btn"><NavLink to="/contact">Contact</NavLink></button></div>
-        <div><button className="btn"><NavLink to="/references"> References</NavLink></button></div>
+        <div><button className="btn"><NavLink activeClassName="selected" exact to="/">Home</NavLink></button></div>
+        <div><button className="btn"><NavLink activeClassName="selected" to="/about">About</NavLink></button></div>
+        <div><button className="btn"><NavLink activeClassName="selected" to="/portfolio">Portfolio</NavLink></button></div>
+        <div><button className="btn"><NavLink activeClassName="selected" to="/contact">Contact</NavLink></button></div>
+        <div><button className="btn"><NavLink activeClassName="selected" to="/references"> References</NavLink></button></div>
       </div>
     )
   }
